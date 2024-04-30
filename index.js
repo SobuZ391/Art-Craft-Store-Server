f729dd39784c8b94e96907af8899ae437b58ba2d
Sobuz391 <monowerhossaisobuz@gmail.com>
2024-04-30 22:32:06 +0600

    created server craft item find one

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
       console.log(result);
       res.send(result)
   })
+  app.get('/myCraftItem/:id',async(req,res)=>{
+    const id =req.params.id;
+    const query ={_id:new ObjectId(id)}
+    const result =await itemCollection.findOne(query);
+    res.send(result);
+  })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
